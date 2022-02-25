c7d25e97a545d06818fa7ff76525f6f47572eaf4
Agon21Misini <misiniagon80@gmail.com>
2022-02-25 23:11:32 +0100

    adds links to Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { Header } from 'components/layout/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Medium Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
+
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         Hello!
       </main>
-
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a className='flex items-center jusitfy-center gap-2' href="https://github.com/ney-l" target="_blank" rel='noopener noreferrer'>
-          Build By Neha Lanjewar
+        <a
+          className="flex items-center justify-center gap-2"
+          href="https://github.com/Agon21Misini"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built By Agon Misini
         </a>
       </footer>
     </div>
   )
 }
-
 export default Home
